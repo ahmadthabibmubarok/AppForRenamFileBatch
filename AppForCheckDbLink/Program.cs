9a8f41db59affc189d99ae004fd6121f6c6d9b49
Ahmad Thabib Mubarok <ahmadthabibmubarok@gmail.com>
2023-03-07 08:48:20 +0700

    init  local
    init  local

---
+++ AppForCheckDbLink/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AppForCheckDbLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                Console.WriteLine("Checking Db Link Hard Code File Started");
+                Console.WriteLine("Please Input Folder Path !");
+                string FolderPath = Console.ReadLine();
+                Console.WriteLine("Please Input Parameter DbLink, default by _DB");
+                string DbLinkParam = Console.ReadLine();
+               
+
+                DirectoryInfo d = new DirectoryInfo(FolderPath);
+                FileInfo[] infos = d.GetFiles();
+                foreach (FileInfo f in infos)
+                {
+
+                    string[] lines = System.IO.File.ReadAllLines(@""+f.DirectoryName+"\\"+f.Name);
+                    for (int i = 0; i < lines.Count(); i++)
+                    {
+                        if (lines[i].ToUpper().Contains("_DB") || lines[i].ToUpper().Contains("DbLinkParam"))
+                        {
+                            int row = i + 1;
+                            Console.WriteLine("Dblink On File (" + f.Name + ") With Line (" + row + ") :"  + lines[i]);
+                        }
+                       
+                    }
+                    
+                }
+
+                Console.WriteLine("Check Dblink Success");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Checking File Error :" + e.Message.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ AppForRenameFile/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForRenameFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Rename File Started");
+                Console.WriteLine("Please Input Folder Path !");
+                string FolderPath = Console.ReadLine();
+                //DirectoryInfo d = new DirectoryInfo(@"D:\Project Fujitsu\COMPARE TABLE AND SP FROM DEPLOYMENT PROCEDURE WITH TRIAL LATEST\TRIAL SP\PEDS_INV_DB_TR_SAPHANA");
+                DirectoryInfo d = new DirectoryInfo(FolderPath);
+                FileInfo[] infos = d.GetFiles();
+                foreach (FileInfo f in infos)
+                {
+                    
+                    File.Move(f.FullName, f.FullName.Replace("dbo.", "").Replace(".StoredProcedure", "").Replace("create_", "").Replace("alter_", "").Replace(".Table", "").Replace(".View", ""));
+                    //File.Move(f.FullName, f.FullName.Replace("create_", "").Replace("alter_", ""));
+                    //File.Move(f.FullName, f.FullName.Replace("dbo.", "").Replace(".Table", ""));
+                }
+                Console.WriteLine("Rename Success");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Rename Error :"+e.Message.ToString());
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
